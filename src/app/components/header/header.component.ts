69cf67fa83feef87f124c9f9929d9bf734f440b3
selcukyazicii <63070617+selcukyazicii@users.noreply.github.com>
2023-07-27 10:19:44 +0300

    constructor created

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  componentName:string="header"; 
+constructor(){}
+
+ngOnInit(): void {
+    this.setTtitle();
+}
+
+  setTtitle(){
+    this.componentName='AAA';
+  }
+
 }
