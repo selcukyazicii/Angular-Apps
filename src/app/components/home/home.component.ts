88d4c206552c1dd7d7c049369563748bb1009e65
selcukyazicii <63070617+selcukyazicii@users.noreply.github.com>
2023-07-28 10:19:25 +0300

    ngFor added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+users:string[]=[''];
+
+constructor(){}
+
+ngOnInit():void{
+this.users=['Selçuk','Yazici'];
+}
 
 }
